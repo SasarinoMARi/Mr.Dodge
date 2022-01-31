146d65e2f89bd7efb5ce25688d6d6b93a5a81f00
Sasarino MARi <develop.mari@gmail.com>
2022-01-31 18:13:51 +0900

    크롤러 작성 및 기본 골자 코딩

---
+++ crawler.js
@@ -0,0 +1,39 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const getHtml = async () => {
+    try {
+        return await axios.get("https://poro.gg/champions?gameMode=aram");
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+module.exports = {
+    stuff: function (callback) {
+        getHtml()
+            .then(html => {
+                let champions = [];
+                const $ = cheerio.load(html.data);
+                const $bodyList = $("div.champion-sub-list__content").children("div.champion-sub-list__item");
+
+                $bodyList.each(function (i, elem) {
+                    if (i > 0)
+
+                        champions[i] = {
+                            rank: $(this).find('div.rank').text(),
+                            name: $(this).find('div.champion a div span').text().trim(),
+                            tier: $(this).find('div:nth-child(3) img').attr("alt"), //.replace("Tier ", "")
+                            winrate: $(this).find('div:nth-child(4)').text().trim(),
+                            pickrate: $(this).find('div:nth-child(5)').text().trim()
+                        };
+                })
+                const data = champions.filter(n => n.name);
+                return data;
+            })
+            .then(data => {
+                //console.log(data);
+                callback(data);
+            });
+    }
+};
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const secret = require('./secret.json');
-const Discord = require('discord.js');
-const client = new Discord.Client({
+const dic = require('./dictionary.json');
+const discord = require('discord.js');
+const crawler = require('./crawler');
+
+const client = new discord.Client({
   intents: [
     'GUILDS',
     'GUILD_MESSAGES'
@@ -13,7 +16,27 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
-      msg.reply('Pong! `' + Math.floor(client.uptime) + ' ms`');
+    msg.reply('Pong! `' + Math.floor(client.uptime) + ' ms`');
+  }
+  if (msg.content.startsWith('!칼')) {
+    // Solving command and champion names
+    const params = msg.content.split(" ");
+    params.shift();
+    let nicknames = dic.filter(x=> params.includes(x.name));
+    nicknames.forEach(function(e) {
+      params.push(e.dest);
+    });
+
+    crawler.stuff(function (data) {
+      console.log("크롤러 반환 완료, 데이터 수 : " + data.length);
+      let res = data.filter(x => params.includes(x.name));
+
+      var builder = "순위\t티어\t이름\t승률\t픽률\n";
+      for(var i= 0; i< res.length; i++) {
+        builder += res[i].rank+'\t' + res[i].tier+'\t' + res[i].name+'\t' + res[i].winrate+'\t' + res[i].pickrate+'\n';
+      }
+      msg.reply(builder);
+    });
   }
 });
 
