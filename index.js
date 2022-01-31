e5df30d9ede9229516907523c86aa771f6f081ab
Sasarino MARi <develop.mari@gmail.com>
2022-01-31 16:09:55 +0900

    노드 프로젝트로 변경

---
+++ index.js
@@ -0,0 +1,21 @@
+const secret = require('./secret.json');
+const Discord = require('discord.js');
+const client = new Discord.Client({
+  intents: [
+    'GUILDS',
+    'GUILD_MESSAGES'
+  ]
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+      msg.reply('Pong! `' + Math.floor(client.uptime) + ' ms`');
+  }
+});
+
+
+client.login(secret.token);
